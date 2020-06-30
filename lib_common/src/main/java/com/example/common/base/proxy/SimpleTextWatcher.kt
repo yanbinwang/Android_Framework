5508fdaedfabb945731077a4374b5c7bcda8b781
wyb <wangyanbin@fenxianglife.com>
2020-06-30 20:47:46 +0800

    剔除黄油刀，改用安卓原生的viewbinding（真香）

---
+++ lib_common/src/main/java/com/example/common/base/proxy/SimpleTextWatcher.kt
@@ -0,0 +1,20 @@
+package com.example.common.base.proxy
+
+import android.text.Editable
+import android.text.TextWatcher
+
+/**
+ * Created by WangYanBin on 2020/6/15.
+ */
+abstract class SimpleTextWatcher : TextWatcher {
+
+    override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+    }
+
+    override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+    }
+
+    override fun afterTextChanged(s: Editable) {
+    }
+
+}
