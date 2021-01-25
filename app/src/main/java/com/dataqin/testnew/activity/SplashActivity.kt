1d9339fb8e909abca7c44e4a41bd357dfedd2093
王彦彬 <wyb@dataqin.com>
2021-01-25 13:56:07 +0800

    升级，替换

---
+++ app/src/main/java/com/dataqin/testnew/activity/SplashActivity.kt
@@ -0,0 +1,27 @@
+package com.dataqin.testnew.activity
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ *  Created by wangyanbin
+ *  app整体启动页
+ *  安卓本身bug会在初次安装应用后点击图标再次拉起启动页，造成界面显示不不正常
+ */
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (!isTaskRoot
+            && intent.hasCategory(Intent.CATEGORY_LAUNCHER)
+            && intent.action != null
+            && intent.action == Intent.ACTION_MAIN) {
+            finish()
+            return
+        }
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+
+}
