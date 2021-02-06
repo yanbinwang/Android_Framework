911a151d5dc814c5eb3a856e072fb9d9d53c735d
王彦彬 <wyb@dataqin.com>
2021-02-06 14:17:07 +0800

    相机类添加

--- app/src/main/java/com/dataqin/testnew/utils/CertificateHelper.kt
+++ app/src/main/java/com/dataqin/testnew/utils/CertificateHelper.kt
@@ -1,17 +1,16 @@
 package com.dataqin.testnew.utils
 
 import android.annotation.SuppressLint
-import android.app.Activity
+import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
 import android.os.Looper
-import android.util.DisplayMetrics
 import android.view.LayoutInflater
 import android.view.View
-import android.widget.TextView
+import com.dataqin.common.constant.Constants
 import com.dataqin.common.utils.handler.WeakHandler
-import com.dataqin.testnew.R
+import com.dataqin.testnew.databinding.ViewCertificateBinding
 import java.util.concurrent.Executors
 
 
@@ -24,59 +23,58 @@ object CertificateHelper {
     private val weakHandler = WeakHandler(Looper.getMainLooper())
     private val executors = Executors.newSingleThreadExecutor()
 
-    fun create(activity: Activity, result: String, onCertificateListener: OnCertificateListener?) {
+    fun create(context: Context, result: String, onCertificateListener: OnCertificateListener?) {
         onCertificateListener?.onStart()
-        val view = LayoutInflater.from(activity).inflate(R.layout.view_certificate, null)
-//        val svContainer = view.findViewById<ScrollView>(R.id.sv_container)
-//        val rlContainer = view.findViewById<RelativeLayout>(R.id.rl_container)
-        val tvContext = view.findViewById<TextView>(R.id.tv_context)
-        tvContext.text = result
-        layoutView(activity, view)
+        val binding = ViewCertificateBinding.inflate(LayoutInflater.from(context))
+        binding.tvContext.text = result
+        loadLayout(binding.root)
         executors.execute {
             try {
-                val bitmap = loadBitmapFromView(view)
-                weakHandler.post {
-                    onCertificateListener?.onResult(bitmap!!)
-                }
+                val bitmap = loadBitmap(binding.root)
+                weakHandler.post { onCertificateListener?.onResult(bitmap!!) }
             } catch (e: Exception) {
             } finally {
-                weakHandler.post {
-                    onCertificateListener?.onComplete()
-                }
+                weakHandler.post { onCertificateListener?.onComplete() }
             }
         }
         executors.isShutdown
     }
 
-    private fun layoutView(activity: Activity, view: View) {
-        val metric = DisplayMetrics()
-        activity.windowManager.defaultDisplay.getMetrics(metric)
-        val width = metric.widthPixels//屏幕宽度（像素）
-        val height = metric.heightPixels//屏幕高度（像素）
-        //整个View的大小 参数是左上角 和右下角的坐标
-        view.layout(0, 0, width, height)
+//    private fun layoutView(activity: Activity, view: View) {
+//        val metric = DisplayMetrics()
+//        activity.windowManager.defaultDisplay.getMetrics(metric)
+//        val width = metric.widthPixels//屏幕宽度（像素）
+//        val height = metric.heightPixels//屏幕高度（像素）
+//        view.layout(0, 0, width, height)
 //        val measuredWidth: Int = View.MeasureSpec.makeMeasureSpec(width, View.MeasureSpec.EXACTLY)
-//        val measuredHeight: Int = View.MeasureSpec.makeMeasureSpec(10000, View.MeasureSpec.AT_MOST)
-        val measuredWidth: Int = View.MeasureSpec.makeMeasureSpec(width, View.MeasureSpec.EXACTLY)
-        val measuredHeight: Int = View.MeasureSpec.makeMeasureSpec(height, View.MeasureSpec.EXACTLY)
-        /**
-         * 当measure完后，并不会实际改变View的尺寸，需要调用View.layout方法去进行布局
-         * 按示例调用layout函数后，View的大小将会变成你想要设置成的大小
-         */
+//        val measuredHeight: Int = View.MeasureSpec.makeMeasureSpec(height, View.MeasureSpec.EXACTLY)
+//        view.measure(measuredWidth, measuredHeight)
+//        view.layout(0, 0, view.measuredWidth, view.measuredHeight)
+//    }
+
+    /**
+     * 当measure完后，并不会实际改变View的尺寸，需要调用View.layout方法去进行布局
+     * 按示例调用layout函数后，View的大小将会变成你想要设置成的大小
+     */
+    private fun loadLayout(view: View) {
+        //整个View的大小 参数是左上角 和右下角的坐标
+        view.layout(0, 0, Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT)
+        val measuredWidth: Int = View.MeasureSpec.makeMeasureSpec(Constants.SCREEN_WIDTH, View.MeasureSpec.EXACTLY)
+        val measuredHeight: Int = View.MeasureSpec.makeMeasureSpec(Constants.SCREEN_HEIGHT, View.MeasureSpec.EXACTLY)
         view.measure(measuredWidth, measuredHeight)
         view.layout(0, 0, view.measuredWidth, view.measuredHeight)
     }
 
-    private fun loadBitmapFromView(view: View): Bitmap? {
-        val w = view.width
-        val h = view.height
-        val bmp = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
-        val c = Canvas(bmp)
-        c.drawColor(Color.WHITE)
-        //如果不设置canvas画布为白色，则生成透明
-        view.layout(0, 0, w, h)
-        view.draw(c)
-        return bmp
+    //如果不设置canvas画布为白色，则生成透明
+    private fun loadBitmap(view: View): Bitmap? {
+        val width = view.width
+        val height = view.height
+        val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        canvas.drawColor(Color.WHITE)
+        view.layout(0, 0, width, height)
+        view.draw(canvas)
+        return bitmap
     }
 
     interface OnCertificateListener {
