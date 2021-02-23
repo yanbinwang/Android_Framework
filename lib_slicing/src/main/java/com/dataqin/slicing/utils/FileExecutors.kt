6946865c427f1baa64242111b20eadf3e4d0fb1b
王彦彬 <wyb@dataqin.com>
2021-02-23 15:46:17 +0800

    规整一下分片逻辑

--- lib_slicing/src/main/java/com/dataqin/slicing/utils/FileExecutors.kt
+++ lib_slicing/src/main/java/com/dataqin/slicing/utils/FileExecutors.kt
@@ -38,58 +38,78 @@ object FileExecutors {
      */
     fun onStart(sourcePath: String, baoquan_no: String, extrasJson: String) {
         val model = SlicingHelper.query(sourcePath)
-        if(null != model){
-            try {
-                //双保险
-                if (!SlicingHelper.isSubmit(model)) {
-                    LogUtil.e(TAG, " \n————————————————————————文件上传————————————————————————\n开始上传:$sourcePath\n————————————————————————文件上传————————————————————————")
+        try {
+            //双保险
+            if (!SlicingHelper.isSubmit(model)) {
+                LogUtil.e(TAG, " \n————————————————————————文件上传————————————————————————\n开始上传:$sourcePath\n————————————————————————文件上传————————————————————————")
+                //是否需要切片
+                if (File(sourcePath).length() >= 100 * 1024 * 1024) {
                     executors.execute {
-                        //是否需要切片
-                        if (File(model.sourcePath).length() >= 100 * 1024 * 1024) {
-                            //通过id查询一下是否有文件切片未提交
-                            var slicingList = SlicingHelper.getSlicingList(model)
-                            //不存在切片则先执行切片插入数据库再获取
-                            if (slicingList.isEmpty() || slicingList.size == 0) {
-                                //先插一条数据并刷出来，切片需要一定的时间
-                                val dbModel = SlicingDBModel(model.sourcePath, AccountHelper.getUserId(), baoquan_no, "",extrasJson, true, true, false)
-                                SlicingHelper.insert(dbModel)
+                        //通过id查询一下是否有文件切片未提交
+                        var slicingList = SlicingHelper.getSlicingList(model)
+                        //不存在切片则先执行切片插入数据库再获取
+                        if (slicingList.isEmpty() || slicingList.size == 0) {
+                            //先插一条数据并刷出来，切片需要一定的时间
+                            val dbModel = SlicingDBModel(sourcePath, AccountHelper.getUserId(), baoquan_no, "", extrasJson, true, true, false)
+                            SlicingHelper.insert(dbModel)
 //                                RxBus.instance.post(RxEvent(Constants.APP_EVIDENCE_EXTRAS_UPDATE))
-                                //再执行分片，切片好后去上传
-                                SlicingHelper.insert(dbModel,true)
-                                slicingList = SlicingHelper.getSlicingList(sourcePath)
-                            } else {
-                                //锁对应id的列表,拿取没传完的整体文件数，接口全走完后再解锁
-                                SlicingHelper.updateSubmit(model, true)
-//                                RxBus.instance.post(RxEvent(Constants.APP_EVIDENCE_EXTRAS_UPDATE))
-                            }
-                            LogUtil.e(TAG, " " + "\n————————————————————————文件上传-分片————————————————————————\n总切片数：" + slicingList.size + "\n未上传完成的数量：" + SlicingHelper.getNotSubmittedCount(slicingList) + "\n————————————————————————文件上传-分片————————————————————————")
-                            //根据path声明一个用于统计请求总数的map集合
-                            frequencyMap[model.sourcePath] = 0
-                            //获取数据库中未传完的分片总数
-                            val threadCount = SlicingHelper.getNotSubmittedCount(slicingList)
-                            //声明一个集合，记录未传完的分片集合
-                            val threadCountList = ArrayList<SlicingModel>()
-                            for (i in slicingList.indices) {
-                                if (!slicingList[i].isSubmit) {
-                                    threadCountList.add(slicingList[i])
-                                }
-                            }
-                            //发起未传完分片的请求，一个完成接口回调后执行下一个
-                            weakHandler.post {
-//                                toPartUpload(threadCountList, model, threadCount, stateList.size)
-                            }
+                            //再执行分片，切片好后去上传
+                            SlicingHelper.insert(dbModel, true)
+                            slicingList = SlicingHelper.getSlicingList(sourcePath)
                         } else {
-                            SlicingHelper.insert(SlicingDBModel(model.sourcePath, AccountHelper.getUserId(), baoquan_no, "",extrasJson, false, true, false))
-//                            RxBus.instance.post(RxEvent(Constants.APP_EVIDENCE_EXTRAS_UPDATE))
-                            //走整体接口
+                            //锁对应id的列表,拿取没传完的整体文件数，接口全走完后再解锁
+                            SlicingHelper.updateSubmit(model!!, true)
+//                                RxBus.instance.post(RxEvent(Constants.APP_EVIDENCE_EXTRAS_UPDATE))
+                        }
+                        LogUtil.e(TAG, " " + "\n————————————————————————文件上传-分片————————————————————————\n总切片数：" + slicingList.size + "\n未上传完成的数量：" + SlicingHelper.getNotSubmittedCount(slicingList) + "\n————————————————————————文件上传-分片————————————————————————")
+                        //根据path声明一个用于统计请求总数的map集合
+                        frequencyMap[sourcePath] = 0
+                        //获取数据库中未传完的分片总数
+                        val threadCount = SlicingHelper.getNotSubmittedCount(slicingList)
+                        //声明一个集合，记录未传完的分片集合
+                        val threadCountList = ArrayList<SlicingModel>()
+                        for (i in slicingList.indices) {
+                            if (!slicingList[i].isSubmit) {
+                                threadCountList.add(slicingList[i])
+                            }
+                        }
+                        //发起未传完分片的请求，一个完成接口回调后执行下一个
+                        weakHandler.post {
+                            toPartUpload(threadCountList, sourcePath, threadCount, slicingList.size)
                         }
                     }
                     executors.isShutdown
                 } else {
-                    LogUtil.e(TAG, " \n————————————————————————文件上传————————————————————————\n正在上传:$sourcePath\n————————————————————————文件上传————————————————————————")
+                    SlicingHelper.insert(SlicingDBModel(sourcePath, AccountHelper.getUserId(), baoquan_no, "", extrasJson, false, true, false))
+//                            RxBus.instance.post(RxEvent(Constants.APP_EVIDENCE_EXTRAS_UPDATE))
+                    //走整体接口
+                    toUpload(sourcePath, baoquan_no)
                 }
-            } catch (e: Exception) {
+            } else {
+                LogUtil.e(TAG, " \n————————————————————————文件上传————————————————————————\n正在上传:$sourcePath\n————————————————————————文件上传————————————————————————")
             }
+        } catch (e: Exception) {
+        }
+    }
+
+    //分片接口
+    private fun toPartUpload(threadCountList: MutableList<SlicingModel>, sourcePath: String = "", threadCount: Int = 0, totalNum: Int = 0) {
+        try {
+            executors.execute {
+
+            }
+            executors.isShutdown
+        } catch (e: Exception) {
+        }
+    }
+
+    private fun toUpload(sourcePath: String, baoquan_no: String) {
+        try {
+            executors.execute {
+
+            }
+            executors.isShutdown
+        } catch (e: Exception) {
         }
     }
 
