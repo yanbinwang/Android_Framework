d6252ccb9080235ee4d4abba5dc8eed7e6008e4c
王彦彬 <wyb@dataqin.com>
2021-02-24 15:14:14 +0800

    规整sd卡工具类

---
+++ app/src/main/java/com/dataqin/testnew/widget/AutoFitTextureView.java
@@ -0,0 +1,52 @@
+package com.dataqin.testnew.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.TextureView;
+
+/**
+ * Created by wangyanbin
+ * 相机容器
+ */
+public class AutoFitTextureView extends TextureView {
+    private int mRatioWidth = 0;
+    private int mRatioHeight = 0;
+
+    public AutoFitTextureView(Context context) {
+        super(context);
+    }
+
+    public AutoFitTextureView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public AutoFitTextureView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int width = MeasureSpec.getSize(widthMeasureSpec);
+        int height = MeasureSpec.getSize(heightMeasureSpec);
+        if (0 == mRatioWidth || 0 == mRatioHeight) {
+            setMeasuredDimension(width, height);
+        } else {
+            if (width < height * mRatioWidth / mRatioHeight) {
+                setMeasuredDimension(width, width * mRatioHeight / mRatioWidth);
+            } else {
+                setMeasuredDimension(height * mRatioWidth / mRatioHeight, height);
+            }
+        }
+    }
+
+    public void setAspectRatio(int width, int height) {
+        if (width < 0 || height < 0) {
+            throw new IllegalArgumentException("Size cannot be negative.");
+        }
+        mRatioWidth = width;
+        mRatioHeight = height;
+        requestLayout();
+    }
+
+}
