d754d262001b1ec7296c14febd01059cc7ed9928
王彦彬 <wyb@dataqin.com>
2021-03-02 14:12:53 +0800

    录像类

--- lib_common/src/main/java/com/dataqin/common/http/factory/OkHttpFactory.kt
+++ lib_common/src/main/java/com/dataqin/common/http/factory/OkHttpFactory.kt
@@ -1,9 +1,7 @@
 package com.dataqin.common.http.factory
 
 import com.dataqin.common.http.interceptor.LoggingInterceptor
-import com.dataqin.common.http.interceptor.RetryServerInterceptor
 import com.dataqin.common.http.interceptor.UserAgentInterceptor
-import okhttp3.ConnectionPool
 import okhttp3.OkHttpClient
 import java.util.concurrent.TimeUnit
 
@@ -23,15 +21,17 @@ class OkHttpFactory private constructor() {
 ////            .addInterceptor(RetryServerInterceptor())//重新构建请求
 //            .connectionPool(ConnectionPool(8, 15, TimeUnit.SECONDS))//根据自己的机型设置同时连接的个数和时间（项目中配置8个，和每个保持时间为15s）
 //            .build()
-    val okHttpClient = OkHttpClient.Builder()
-        .connectTimeout(6, TimeUnit.SECONDS)//设置连接超时
-        .writeTimeout(2, TimeUnit.HOURS)//设置写超时
-        .readTimeout(2, TimeUnit.HOURS)//设置读超时
-        .retryOnConnectionFailure(true)
-        .addInterceptor(UserAgentInterceptor())//请求加头
-        .addInterceptor(LoggingInterceptor())//日志监听
+    val okHttpClient by lazy {
+        OkHttpClient.Builder()
+            .connectTimeout(6, TimeUnit.SECONDS)//设置连接超时
+            .writeTimeout(2, TimeUnit.HOURS)//设置写超时
+            .readTimeout(2, TimeUnit.HOURS)//设置读超时
+            .retryOnConnectionFailure(true)
+            .addInterceptor(UserAgentInterceptor())//请求加头
+            .addInterceptor(LoggingInterceptor())//日志监听
 //            .addInterceptor(RetryServerInterceptor())//重新构建请求
-        .build()
+            .build()
+    }
 
     companion object {
         @JvmStatic
