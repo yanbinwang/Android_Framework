24a66306fae747d98ec5c6428e11330a1a656c46
王彦彬 <wyb@dataqin.com>
2021-03-03 15:22:54 +0800

    录像类

--- app/src/main/java/com/dataqin/testnew/activity/MainActivity.kt
+++ app/src/main/java/com/dataqin/testnew/activity/MainActivity.kt
@@ -41,36 +41,6 @@ class MainActivity : BaseActivity<ActivityMainBinding>(), View.OnClickListener {
                             hideDialog()
                         }
                     })
-
-//                CertificateHelper.create(
-//                    activity.get()!!,
-//                    "嗷嘮啊啦啦啦啦大師傅似的",
-//                    object : CertificateHelper.OnCertificateListener {
-//                        override fun onStart() {
-//                            showDialog()
-//                        }
-//
-//                        override fun onResult(bitmap: Bitmap) {
-////                        val mediaStorageDir = File(
-////                            Environment.getExternalStoragePublicDirectory(
-////                                Environment.DIRECTORY_PICTURES
-////                            ), Constants.APPLICATION_NAME + "/" + CAMERA_FILE_PATH
-////                        )
-////                        val timeStamp =
-////                            SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(
-////                                Date()
-////                            )
-////                        val path =
-////                            File(mediaStorageDir.path + File.separator + timeStamp + ".jpg").path
-//                            val path = MediaFileHelper.getOutputMediaFile(MEDIA_TYPE_IMAGE, Constants.APPLICATION_NAME + "/" + CAMERA_FILE_PATH)?.path
-//                            MediaFileHelper.saveBitmapToSd(bitmap, path, 100)
-//                            binding.ivCertificate.setImageBitmap(bitmap)
-//                        }
-//
-//                        override fun onComplete() {
-//                            hideDialog()
-//                        }
-//                    })
             }
         }
     }
