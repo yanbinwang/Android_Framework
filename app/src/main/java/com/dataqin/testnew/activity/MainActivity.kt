93b328ff38d9a2231298c3a4195a340ebfdbf4e5
王彦彬 <wyb@dataqin.com>
2021-03-04 16:42:44 +0800

    自定义布局的扩展

--- app/src/main/java/com/dataqin/testnew/activity/MainActivity.kt
+++ app/src/main/java/com/dataqin/testnew/activity/MainActivity.kt
@@ -1,62 +1,54 @@
 package com.dataqin.testnew.activity
 
-import android.graphics.Bitmap
-import android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE
 import android.view.View
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.dataqin.common.base.BaseActivity
-import com.dataqin.common.base.BaseTitleActivity
 import com.dataqin.common.constant.ARouterPath
-import com.dataqin.common.constant.Constants
-import com.dataqin.common.constant.Constants.CAMERA_FILE_PATH
-import com.dataqin.media.utils.helper.MediaFileHelper
 import com.dataqin.testnew.R
 import com.dataqin.testnew.databinding.ActivityMainBinding
-import com.dataqin.testnew.utils.CertificateHelper
 
 /**
  * Created by WangYanBin
  */
 @Route(path = ARouterPath.MainActivity)
-class MainActivity : BaseTitleActivity<ActivityMainBinding>(), View.OnClickListener {
+class MainActivity : BaseActivity<ActivityMainBinding>(), View.OnClickListener {
 
     override fun initView() {
         super.initView()
-        showError()
     }
 
     override fun onClick(v: View?) {
         when (v?.id) {
             R.id.btn_create -> {
-                CertificateHelper.create(
-                    activity.get()!!,
-                    "嗷嘮啊啦啦啦啦大師傅似的",
-                    object : CertificateHelper.OnCertificateListener {
-                        override fun onStart() {
-                            showDialog()
-                        }
-
-                        override fun onResult(bitmap: Bitmap) {
-//                        val mediaStorageDir = File(
-//                            Environment.getExternalStoragePublicDirectory(
-//                                Environment.DIRECTORY_PICTURES
-//                            ), Constants.APPLICATION_NAME + "/" + CAMERA_FILE_PATH
-//                        )
-//                        val timeStamp =
-//                            SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(
-//                                Date()
-//                            )
-//                        val path =
-//                            File(mediaStorageDir.path + File.separator + timeStamp + ".jpg").path
-                            val path = MediaFileHelper.getOutputMediaFile(MEDIA_TYPE_IMAGE, Constants.APPLICATION_NAME + "/" + CAMERA_FILE_PATH)?.path
-                            MediaFileHelper.saveBitmapToSd(bitmap, path, 100)
-                            binding.ivCertificate.setImageBitmap(bitmap)
-                        }
-
-                        override fun onComplete() {
-                            hideDialog()
-                        }
-                    })
+//                CertificateHelper.create(
+//                    activity.get()!!,
+//                    "嗷嘮啊啦啦啦啦大師傅似的",
+//                    object : CertificateHelper.OnCertificateListener {
+//                        override fun onStart() {
+//                            showDialog()
+//                        }
+//
+//                        override fun onResult(bitmap: Bitmap) {
+////                        val mediaStorageDir = File(
+////                            Environment.getExternalStoragePublicDirectory(
+////                                Environment.DIRECTORY_PICTURES
+////                            ), Constants.APPLICATION_NAME + "/" + CAMERA_FILE_PATH
+////                        )
+////                        val timeStamp =
+////                            SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(
+////                                Date()
+////                            )
+////                        val path =
+////                            File(mediaStorageDir.path + File.separator + timeStamp + ".jpg").path
+//                            val path = MediaFileHelper.getOutputMediaFile(MEDIA_TYPE_IMAGE, Constants.APPLICATION_NAME + "/" + CAMERA_FILE_PATH)?.path
+//                            MediaFileHelper.saveBitmapToSd(bitmap, path, 100)
+//                            binding.ivCertificate.setImageBitmap(bitmap)
+//                        }
+//
+//                        override fun onComplete() {
+//                            hideDialog()
+//                        }
+//                    })
             }
         }
     }
