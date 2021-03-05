f3ed005df25954b90a4751b11fb3510bd44530af
王彦彬 <wyb@dataqin.com>
2021-03-05 16:42:07 +0800

    剔除淘汰的方法

--- app/src/main/java/com/dataqin/testnew/activity/MainActivity.kt
+++ app/src/main/java/com/dataqin/testnew/activity/MainActivity.kt
@@ -5,6 +5,9 @@ import android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE
 import android.view.LayoutInflater
 import android.view.View
 import com.alibaba.android.arouter.facade.annotation.Route
+import com.app.hubert.guide.NewbieGuide
+import com.app.hubert.guide.model.GuidePage
+import com.app.hubert.guide.model.HighlightOptions
 import com.dataqin.common.base.BaseActivity
 import com.dataqin.common.constant.ARouterPath
 import com.dataqin.common.constant.Constants
@@ -15,24 +18,47 @@ import com.dataqin.testnew.R
 import com.dataqin.testnew.databinding.ActivityMainBinding
 import com.dataqin.testnew.databinding.ViewCertificateBinding
 
+
 /**
  * Created by WangYanBin
  */
 @Route(path = ARouterPath.MainActivity)
 class MainActivity : BaseActivity<ActivityMainBinding>(), View.OnClickListener {
 
+    override fun initView() {
+        super.initView()
+
+        //https://github.com/huburt-Hu/NewbieGuide
+        NewbieGuide.with(this)//传入activity
+            .setLabel("guide1")//设置引导层标示，用于区分不同引导层，必传！否则报错
+            .addGuidePage(GuidePage.newInstance()
+                    .setLayoutRes(R.layout.view_guide_step_1)
+                    .addHighLightWithOptions(
+                        binding.btnCreate2, HighlightOptions.Builder()
+                            .setOnClickListener {
+                                showToast("highlight click")
+                            }.build()))
+            .alwaysShow(true)
+            .show()//显示引导层
+    }
+
     override fun onClick(v: View?) {
         when (v?.id) {
             R.id.btn_create -> {
                 val certificateBinding = ViewCertificateBinding.inflate(LayoutInflater.from(this))
                 certificateBinding.tvContext.text = "嗷嘮啊啦啦啦啦大師傅似的"
-                GenerateHelper.create(certificateBinding.root, object : GenerateHelper.OnGenerateListener {
+                GenerateHelper.create(
+                    certificateBinding.root,
+                    object : GenerateHelper.OnGenerateListener {
                         override fun onStart() {
                             showDialog()
                         }
 
                         override fun onResult(bitmap: Bitmap) {
-                            val path = MediaFileHelper.getOutputMediaFile(MEDIA_TYPE_IMAGE, Constants.APPLICATION_NAME + "/" + CAMERA_FILE_PATH)?.path
+                            val path = MediaFileHelper.getOutputMediaFile(
+                                MEDIA_TYPE_IMAGE,
+                                Constants.APPLICATION_NAME + "/" + CAMERA_FILE_PATH
+                            )?.path
                             MediaFileHelper.saveBitmapToSd(bitmap, path, 100)
                             binding.ivCertificate.setImageBitmap(bitmap)
                         }
