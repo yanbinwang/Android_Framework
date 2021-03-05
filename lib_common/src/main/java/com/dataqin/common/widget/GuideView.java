13b6204e4757bb15ec017f41c639ab58ec3d31bb
王彦彬 <wyb@dataqin.com>
2021-03-05 09:27:48 +0800

    整理代码逻辑

--- lib_common/src/main/java/com/dataqin/common/widget/GuideView.java
+++ lib_common/src/main/java/com/dataqin/common/widget/GuideView.java
@@ -70,144 +70,166 @@ import com.dataqin.common.R;
  *      }
  *  }).build();
  */
-@SuppressLint("ApplySharedPref")
+@SuppressLint({"ApplySharedPref","StaticFieldLeak"})
 public class GuideView extends RelativeLayout implements ViewTreeObserver.OnGlobalLayoutListener {
-    private boolean first = true;
-    private boolean onClickExit;
+    // <editor-fold defaultstate="collapsed" desc="基础属性和枚举类">
+    private boolean isFirst = true;//是否第一次加载
     private boolean isMeasured;//targetView是否已测量
-    private int offsetX, offsetY;//GuideView 偏移量
+    private boolean onClickExit;//点击退出
     private int radius;//targetView 的外切圆半径
     private int backgroundColor;//背景色和透明度，格式 #aarrggbb
+    private int offsetX, offsetY;//GuideView 偏移量
     private int[] center;//targetView圆心
     private int[] location;//targetView左上角坐标
     private View targetView;//需要显示提示信息的View
     private View customGuideView;//自定义View
     private Paint mCirclePaint;//透明圆形画笔
     private PorterDuffXfermode porterDuffXfermode;//绘图层叠模式
-    private Bitmap bitmap;//绘制前景bitmap
-    private OnClickCallback onclickListener;
     private Canvas temp;//Canvas,绘制bitmap
+    private Bitmap bitmap;//绘制前景bitmap
     private Direction direction;//相对于targetView的位置.在target的那个方向
     private MyShape myShape;//形状
+    private OnClickCallback onclickListener;
     private final Context mContent;
     private final String TAG = getClass().getSimpleName();
     private static final String SHOW_GUIDE_PREFIX = "show_guide_on_view_";//targetView前缀。SHOW_GUIDE_PREFIX + targetView.getId()作为保存在SP文件的key。
+    /**
+     * 定义GuideView相对于targetView的方位，共八种。不设置则默认在targetView下方
+     */
+    enum Direction {
+        LEFT, TOP, RIGHT, BOTTOM,
+        LEFT_TOP, LEFT_BOTTOM,
+        RIGHT_TOP, RIGHT_BOTTOM
+    }
 
+    /**
+     * 定义目标控件的形状，共3种。圆形，椭圆，带圆角的矩形（可以设置圆角大小），不设置则默认是圆形
+     */
+    enum MyShape {
+        CIRCULAR, ELLIPSE, RECTANGULAR
+    }
+    // </editor-fold>
+
+    // <editor-fold defaultstate="collapsed" desc="基础方法">
     public GuideView(Context context) {
         super(context);
         this.mContent = context;
     }
 
-    public void restoreState() {
-        LogUtil.v(TAG, "restoreState");
-        offsetX = offsetY = 0;
-        radius = 0;
-        mCirclePaint = null;
-        isMeasured = false;
-        center = null;
-        porterDuffXfermode = null;
-        bitmap = null;
-        temp = null;
-    }
-
-    public int[] getLocation() {
-        return location;
-    }
-
-    public void setLocation(int[] location) {
-        this.location = location;
-    }
-
-    public int getRadius() {
-        return radius;
-    }
-
-    public void setRadius(int radius) {
-        this.radius = radius;
-    }
-
-    public void setOffsetX(int offsetX) {
-        this.offsetX = offsetX;
-    }
-
-    public void setOffsetY(int offsetY) {
-        this.offsetY = offsetY;
-    }
-
-    public void setDirection(Direction direction) {
-        this.direction = direction;
-    }
-
-    public void setShape(MyShape shape) {
-        this.myShape = shape;
-    }
-
-    public void setCustomGuideView(View customGuideView) {
-        this.customGuideView = customGuideView;
-        if (!first) {
-            restoreState();
-        }
-    }
-
-    public void setBgColor(int background_color) {
-        this.backgroundColor = background_color;
-    }
-
-    public View getTargetView() {
-        return targetView;
-    }
-
-    public void setTargetView(View targetView) {
-        this.targetView = targetView;
-    }
-
-    public void showOnce() {
-        if (targetView != null) {
-            mContent.getSharedPreferences(TAG, Context.MODE_PRIVATE).edit().putBoolean(generateUniqId(targetView), true).commit();
-        }
-    }
-
-    private boolean hasShown() {
-        if (targetView == null)
-            return true;
-        return mContent.getSharedPreferences(TAG, Context.MODE_PRIVATE).getBoolean(generateUniqId(targetView), false);
-    }
-
-    private String generateUniqId(View v) {
-        return SHOW_GUIDE_PREFIX + v.getId();
-    }
-
-    public int[] getCenter() {
-        return center;
-    }
-
-    public void setCenter(int[] center) {
-        this.center = center;
-    }
-
-    public void hide() {
-        LogUtil.v(TAG, "hide");
-        if (customGuideView != null) {
-            targetView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
-            this.removeAllViews();
-            ((FrameLayout) ((Activity) mContent).getWindow().getDecorView()).removeView(this);
-            restoreState();
-        }
-    }
-
-    public void show() {
-        LogUtil.v(TAG, "show");
-        if (hasShown())
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        LogUtil.v(TAG, "onDraw");
+        if (!isMeasured)
             return;
+        if (targetView == null)
+            return;
+        drawBackground(canvas);
+    }
 
-        if (targetView != null) {
-            targetView.getViewTreeObserver().addOnGlobalLayoutListener(this);
+    private void drawBackground(Canvas canvas) {
+        LogUtil.v(TAG, "drawBackground");
+        // 先绘制bitmap，再将bitmap绘制到屏幕
+        bitmap = Bitmap.createBitmap(canvas.getWidth(), canvas.getHeight(), Bitmap.Config.ARGB_8888);
+        temp = new Canvas(bitmap);
+        // 背景画笔
+        Paint bgPaint = new Paint();
+        if (backgroundColor != 0)
+            bgPaint.setColor(backgroundColor);
+        else
+            bgPaint.setColor(getResources().getColor(R.color.shadow));
+        // 绘制屏幕背景
+        temp.drawRect(0, 0, temp.getWidth(), temp.getHeight(), bgPaint);
+        // targetView 的透明圆形画笔
+        if (mCirclePaint == null)
+            mCirclePaint = new Paint();
+        porterDuffXfermode = new PorterDuffXfermode(PorterDuff.Mode.SRC_OUT);// 或者CLEAR
+        mCirclePaint.setXfermode(porterDuffXfermode);
+        mCirclePaint.setAntiAlias(true);
+        if (myShape != null) {
+            RectF oval = new RectF();
+            switch (myShape) {
+                case CIRCULAR://圆形
+                    temp.drawCircle(center[0], center[1], radius, mCirclePaint);//绘制圆形
+                    break;
+                case ELLIPSE://椭圆
+                    //RectF对象
+                    oval.left = center[0] - 150;//左边
+                    oval.top = center[1] - 50;//上边
+                    oval.right = center[0] + 150;//右边
+                    oval.bottom = center[1] + 50;//下边
+                    temp.drawOval(oval, mCirclePaint);//绘制椭圆
+                    break;
+                case RECTANGULAR://圆角矩形
+                    //RectF对象
+                    oval.left = center[0] - 150;//左边
+                    oval.top = center[1] - 50;//上边
+                    oval.right = center[0] + 150;//右边
+                    oval.bottom = center[1] + 50;//下边
+                    temp.drawRoundRect(oval, radius, radius, mCirclePaint);//绘制圆角矩形
+                    break;
+            }
+        } else {
+            temp.drawCircle(center[0], center[1], radius, mCirclePaint);//绘制圆形
+        }
+        // 绘制到屏幕
+        canvas.drawBitmap(bitmap, 0, 0, bgPaint);
+        bitmap.recycle();
+    }
+
+    @Override
+    public void onGlobalLayout() {
+        if (isMeasured)
+            return;
+        if (targetView.getHeight() > 0 && targetView.getWidth() > 0) {
+            isMeasured = true;
         }
 
-        this.setBackgroundResource(R.color.transparent);
+        // 获取targetView的中心坐标
+        if (center == null) {
+            // 获取右上角坐标
+            location = new int[2];
+            targetView.getLocationInWindow(location);
+            center = new int[2];
+            // 获取中心坐标
+            center[0] = location[0] + targetView.getWidth() / 2;
+            center[1] = location[1] + targetView.getHeight() / 2;
+        }
+        // 获取targetView外切圆半径
+        if (radius == 0) {
+            radius = getTargetViewRadius();
+        }
+        // 添加GuideView
+        createGuideView();
+    }
 
-        ((FrameLayout) ((Activity) mContent).getWindow().getDecorView()).addView(this);
-        first = false;
+    /**
+     * 获得targetView 的半径
+     *
+     * @return
+     */
+    private int getTargetViewRadius() {
+        if (isMeasured) {
+            int[] size = getTargetViewSize();
+            int x = size[0];
+            int y = size[1];
+            return (int) (Math.sqrt(x * x + y * y) / 2);
+        }
+        return -1;
+    }
+
+    /**
+     * 获得targetView 的宽高，如果未测量，返回｛-1， -1｝
+     *
+     * @return
+     */
+    private int[] getTargetViewSize() {
+        int[] location = {-1, -1};
+        if (isMeasured) {
+            location[0] = targetView.getWidth();
+            location[1] = targetView.getHeight();
+        }
+        return location;
     }
 
     /**
@@ -267,96 +289,116 @@ public class GuideView extends RelativeLayout implements ViewTreeObserver.OnGlob
             this.addView(customGuideView, guideViewParams);
         }
     }
+    // </editor-fold>
 
-    /**
-     * 获得targetView 的宽高，如果未测量，返回｛-1， -1｝
-     *
-     * @return
-     */
-    private int[] getTargetViewSize() {
-        int[] location = {-1, -1};
-        if (isMeasured) {
-            location[0] = targetView.getWidth();
-            location[1] = targetView.getHeight();
-        }
+    // <editor-fold defaultstate="collapsed" desc="公共方法">
+    public int[] getLocation() {
         return location;
     }
 
-    /**
-     * 获得targetView 的半径
-     *
-     * @return
-     */
-    private int getTargetViewRadius() {
-        if (isMeasured) {
-            int[] size = getTargetViewSize();
-            int x = size[0];
-            int y = size[1];
-            return (int) (Math.sqrt(x * x + y * y) / 2);
-        }
-        return -1;
+    public void setLocation(int[] location) {
+        this.location = location;
     }
 
+    public int getRadius() {
+        return radius;
+    }
 
-    @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-        LogUtil.v(TAG, "onDraw");
-        if (!isMeasured)
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    public void setOffsetX(int offsetX) {
+        this.offsetX = offsetX;
+    }
+
+    public void setOffsetY(int offsetY) {
+        this.offsetY = offsetY;
+    }
+
+    public void setDirection(Direction direction) {
+        this.direction = direction;
+    }
+
+    public void setShape(MyShape shape) {
+        this.myShape = shape;
+    }
+
+    public void setCustomGuideView(View customGuideView) {
+        this.customGuideView = customGuideView;
+        if (!isFirst) {
+            restoreState();
+        }
+    }
+
+    public void setBgColor(int background_color) {
+        this.backgroundColor = background_color;
+    }
+
+    public View getTargetView() {
+        return targetView;
+    }
+
+    public void setTargetView(View targetView) {
+        this.targetView = targetView;
+    }
+
+    private String generateUniqId(View v) {
+        return SHOW_GUIDE_PREFIX + v.getId();
+    }
+
+    public int[] getCenter() {
+        return center;
+    }
+
+    public void setCenter(int[] center) {
+        this.center = center;
+    }
+
+    public void restoreState() {
+        LogUtil.v(TAG, "restoreState");
+        offsetX = offsetY = 0;
+        radius = 0;
+        mCirclePaint = null;
+        isMeasured = false;
+        center = null;
+        porterDuffXfermode = null;
+        bitmap = null;
+        temp = null;
+    }
+
+    public void show() {
+        LogUtil.v(TAG, "show");
+        if (hasShown())
             return;
+        if (targetView != null) {
+            targetView.getViewTreeObserver().addOnGlobalLayoutListener(this);
+        }
+        this.setBackgroundResource(R.color.transparent);
+        ((FrameLayout) ((Activity) mContent).getWindow().getDecorView()).addView(this);
+        isFirst = false;
+    }
+
+    private boolean hasShown() {
         if (targetView == null)
-            return;
-        drawBackground(canvas);
+            return true;
+        return mContent.getSharedPreferences(TAG, Context.MODE_PRIVATE).getBoolean(generateUniqId(targetView), false);
     }
 
-    private void drawBackground(Canvas canvas) {
-        LogUtil.v(TAG, "drawBackground");
-        // 先绘制bitmap，再将bitmap绘制到屏幕
-        bitmap = Bitmap.createBitmap(canvas.getWidth(), canvas.getHeight(), Bitmap.Config.ARGB_8888);
-        temp = new Canvas(bitmap);
-        // 背景画笔
-        Paint bgPaint = new Paint();
-        if (backgroundColor != 0)
-            bgPaint.setColor(backgroundColor);
-        else
-            bgPaint.setColor(getResources().getColor(R.color.shadow));
-        // 绘制屏幕背景
-        temp.drawRect(0, 0, temp.getWidth(), temp.getHeight(), bgPaint);
-        // targetView 的透明圆形画笔
-        if (mCirclePaint == null)
-            mCirclePaint = new Paint();
-        porterDuffXfermode = new PorterDuffXfermode(PorterDuff.Mode.SRC_OUT);// 或者CLEAR
-        mCirclePaint.setXfermode(porterDuffXfermode);
-        mCirclePaint.setAntiAlias(true);
-        if (myShape != null) {
-            RectF oval = new RectF();
-            switch (myShape) {
-                case CIRCULAR://圆形
-                    temp.drawCircle(center[0], center[1], radius, mCirclePaint);//绘制圆形
-                    break;
-                case ELLIPSE://椭圆
-                    //RectF对象
-                    oval.left = center[0] - 150;                              //左边
-                    oval.top = center[1] - 50;                                   //上边
-                    oval.right = center[0] + 150;                             //右边
-                    oval.bottom = center[1] + 50;                                //下边
-                    temp.drawOval(oval, mCirclePaint);                   //绘制椭圆
-                    break;
-                case RECTANGULAR://圆角矩形
-                    //RectF对象
-                    oval.left = center[0] - 150;                              //左边
-                    oval.top = center[1] - 50;                                   //上边
-                    oval.right = center[0] + 150;                             //右边
-                    oval.bottom = center[1] + 50;                                //下边
-                    temp.drawRoundRect(oval, radius, radius, mCirclePaint);                   //绘制圆角矩形
-                    break;
-            }
-        } else {
-            temp.drawCircle(center[0], center[1], radius, mCirclePaint);//绘制圆形
+    public void showOnce() {
+        if (targetView != null) {
+            mContent.getSharedPreferences(TAG, Context.MODE_PRIVATE).edit().putBoolean(generateUniqId(targetView), true).commit();
         }
-        // 绘制到屏幕
-        canvas.drawBitmap(bitmap, 0, 0, bgPaint);
-        bitmap.recycle();
+    }
+
+    public void hide() {
+        LogUtil.v(TAG, "hide");
+        if (customGuideView != null) {
+            targetView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+            this.removeAllViews();
+            ((FrameLayout) ((Activity) mContent).getWindow().getDecorView()).removeView(this);
+            restoreState();
+        }
     }
 
     public void setOnClickExit(boolean onClickExit) {
@@ -366,61 +408,9 @@ public class GuideView extends RelativeLayout implements ViewTreeObserver.OnGlob
     public void setOnclickListener(OnClickCallback onclickListener) {
         this.onclickListener = onclickListener;
     }
+    // </editor-fold>
 
-    private void setClickInfo() {
-        final boolean exit = onClickExit;
-        setOnClickListener(v -> {
-            if (onclickListener != null) {
-                onclickListener.onClickedGuideView();
-            }
-            if (exit) {
-                hide();
-            }
-        });
-    }
-
-    @Override
-    public void onGlobalLayout() {
-        if (isMeasured)
-            return;
-        if (targetView.getHeight() > 0 && targetView.getWidth() > 0) {
-            isMeasured = true;
-        }
-
-        // 获取targetView的中心坐标
-        if (center == null) {
-            // 获取右上角坐标
-            location = new int[2];
-            targetView.getLocationInWindow(location);
-            center = new int[2];
-            // 获取中心坐标
-            center[0] = location[0] + targetView.getWidth() / 2;
-            center[1] = location[1] + targetView.getHeight() / 2;
-        }
-        // 获取targetView外切圆半径
-        if (radius == 0) {
-            radius = getTargetViewRadius();
-        }
-        // 添加GuideView
-        createGuideView();
-    }
-
-    /**
-     * 定义GuideView相对于targetView的方位，共八种。不设置则默认在targetView下方
-     */
-    enum Direction {
-        LEFT, TOP, RIGHT, BOTTOM,
-        LEFT_TOP, LEFT_BOTTOM,
-        RIGHT_TOP, RIGHT_BOTTOM
-    }
-
-    /**
-     * 定义目标控件的形状，共3种。圆形，椭圆，带圆角的矩形（可以设置圆角大小），不设置则默认是圆形
-     */
-    enum MyShape {
-        CIRCULAR, ELLIPSE, RECTANGULAR
-    }
-
+    // <editor-fold defaultstate="collapsed" desc="监听和构造方法">
     /**
      * GuideView点击Callback
      */
@@ -428,17 +418,11 @@ public class GuideView extends RelativeLayout implements ViewTreeObserver.OnGlob
         void onClickedGuideView();
     }
 
-    @SuppressLint("StaticFieldLeak")
     public static class Builder {
         private static GuideView guiderView;
-        private static Builder instance = new Builder();
-        private Context mContext;
+        private static final Builder instance = new Builder();
 
         private Builder() {
-        }
-
-        public Builder(Context ctx) {
-            mContext = ctx;
         }
 
         public static Builder newInstance(Context ctx) {
@@ -508,4 +492,16 @@ public class GuideView extends RelativeLayout implements ViewTreeObserver.OnGlob
         }
     }
 
+    private void setClickInfo() {
+        final boolean exit = onClickExit;
+        setOnClickListener(v -> {
+            if (onclickListener != null) {
+                onclickListener.onClickedGuideView();
+            }
+            if (exit) {
+                hide();
+            }
+        });
+    }
+    // </editor-fold>
 }
