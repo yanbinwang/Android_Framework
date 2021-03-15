5452d1c9d52fa22ff9fd7b3c1449b362272fe172
王彦彬 <wyb@dataqin.com>
2021-03-15 15:22:14 +0800

    相机库的替换，休整

--- lib_common/src/main/java/com/dataqin/common/utils/helper/FragmentHelper.kt
+++ lib_common/src/main/java/com/dataqin/common/utils/helper/FragmentHelper.kt
@@ -12,17 +12,15 @@ import java.lang.ref.WeakReference
  */
 object FragmentHelper {
     private var containerViewId = 0
-    private var tagList = ArrayList<String>()
     private var fragmentList = ArrayList<Fragment>()
     private var weakActivity: WeakReference<AppCompatActivity>? = null
     var onTabClickListener: OnTabClickListener? = null
 
     @JvmStatic
-    fun initialize(activity: AppCompatActivity, containerViewId: Int, tagList: Array<String>, fragmentList: ArrayList<Fragment>, isDark: Boolean = true, tabNum: Int = 0) {
-        weakActivity = WeakReference(activity)
-        FragmentHelper.containerViewId = containerViewId
-        FragmentHelper.tagList = tagList.toList() as ArrayList<String>
-        FragmentHelper.fragmentList = fragmentList
+    fun initialize(activity: AppCompatActivity, containerViewId: Int, fragmentList: ArrayList<Fragment>, isDark: Boolean = true, tabNum: Int = 0) {
+        this.weakActivity = WeakReference(activity)
+        this.containerViewId = containerViewId
+        this.fragmentList = fragmentList
         //默认选中下标以及导航栏颜色
         StatusBarBuilder(activity).setTransparent(isDark)
         showFragment(tabNum, true)
@@ -30,21 +28,23 @@ object FragmentHelper {
 
     @JvmStatic
     fun showFragment(tabNum: Int, load: Boolean = false) {
-        //commit只能提交一次，所以每次都需要重新实例化
-        val fragmentManager = weakActivity?.get()?.supportFragmentManager!!
-        val fragmentTransaction = fragmentManager.beginTransaction()
-        if (load) {
-            for (i in fragmentList.indices) {
-                fragmentTransaction.add(containerViewId, fragmentList[i], tagList[i])
+        if (fragmentList.size > tabNum) {
+            //commit只能提交一次，所以每次都需要重新实例化
+            val fragmentManager = weakActivity?.get()?.supportFragmentManager!!
+            val fragmentTransaction = fragmentManager.beginTransaction()
+            if (load) {
+                for (i in fragmentList.indices) {
+                    fragmentTransaction.add(containerViewId, fragmentList[i])
+                }
             }
+            //全部隱藏后显示指定的页面
+            for (fragment in fragmentList) {
+                fragmentTransaction.hide(fragment)
+            }
+            fragmentTransaction.show(fragmentList[tabNum])
+            fragmentTransaction.commitAllowingStateLoss()
+            onTabClickListener?.onTabClickListener(tabNum)
         }
-        //全部隱藏后显示指定的页面
-        for (fragment in fragmentList) {
-            fragmentTransaction.hide(fragment)
-        }
-        fragmentTransaction.show(fragmentList[tabNum])
-        fragmentTransaction.commitAllowingStateLoss()
-        onTabClickListener?.onTabClickListener(tabNum)
     }
 
     interface OnTabClickListener {
