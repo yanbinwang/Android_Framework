aa300064121379b4adcf1cd4be4fa2cc22dbf42a
王彦彬 <wyb@dataqin.com>
2021-03-19 09:37:18 +0800

    剔除相册的开发库

--- app/src/main/java/com/dataqin/testnew/MyApplication.kt
+++ app/src/main/java/com/dataqin/testnew/MyApplication.kt
@@ -60,14 +60,6 @@ class MyApplication : BaseApplication() {
                 } catch (e: Throwable) {
                     val stackTraceString = Log.getStackTraceString(e)
                     println("AppCatch -$stackTraceString")
-//                    when (e) {
-//                        is NullPointerException -> println("AppCatch -$stackTraceString")
-//                        is IllegalStateException -> println("AppCatch -$stackTraceString")
-//                        is ArrayIndexOutOfBoundsException -> println("AppCatch -$stackTraceString")
-//                        is IndexOutOfBoundsException -> println("AppCatch -$stackTraceString")
-//                        is OutOfMemoryError -> println("AppCatch -$stackTraceString")
-//                        is NumberFormatException -> println("AppCatch -$stackTraceString")
-//                    }
                 }
             }
         }
