73e6ac6c9bc710cc432d7610939e7733583d5932
王彦彬 <wyb@dataqin.com>
2021-04-06 16:18:35 +0800

    合并启动页

---
+++ lib_push/src/main/java/com/dataqin/push/activity/PushActivity.kt
@@ -0,0 +1,12 @@
+package com.dataqin.push.activity
+
+import androidx.viewbinding.ViewBinding
+import com.dataqin.common.base.BaseActivity
+
+/**
+ *  Created by wangyanbin
+ *  无任何页面容器的activity，用于区分跳转的推送页面
+ */
+class PushActivity :BaseActivity<ViewBinding>(){
+
+}
