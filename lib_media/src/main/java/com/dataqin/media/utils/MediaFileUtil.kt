051057bb5d3ba13864b4588e761cf6aaf487861a
王彦彬 <wyb@dataqin.com>
2021-04-22 14:10:11 +0800

    字体引用规整

--- lib_media/src/main/java/com/dataqin/media/utils/MediaFileUtil.kt
+++ lib_media/src/main/java/com/dataqin/media/utils/MediaFileUtil.kt
@@ -4,13 +4,12 @@ import android.graphics.Bitmap
 import android.os.Environment
 import android.provider.MediaStore
 import com.dataqin.base.utils.DateUtil
+import com.dataqin.base.utils.DateUtil.EN_YMDHMS
 import com.dataqin.base.utils.LogUtil
 import com.dataqin.base.utils.SdcardUtil
 import com.dataqin.common.constant.Constants
 import java.io.File
-import java.io.FileNotFoundException
 import java.io.FileOutputStream
-import java.io.IOException
 import java.util.*
 
 /**
@@ -18,10 +17,6 @@ import java.util.*
  *  相机文件管理工具类
  */
 object MediaFileUtil {
-    private const val CAMERA_FILE_PATH = "Photo"
-    private const val VIDEO_FILE_PATH = "Video"
-    private const val AUDIO_FILE_PATH = "Audio"
-    private const val SCREEN_FILE_PATH = "Screen"
     private const val TAG = "MediaFileUtil"
 
     //获取对应文件类型的存储地址
@@ -31,27 +26,28 @@ object MediaFileUtil {
             LogUtil.e(TAG, "can not get sdcard!")
             return null
         }
+        //根据类型在sd卡picture目录下建立对应app名称的对应类型文件
         var prefix = Constants.APPLICATION_NAME + "/"
         var suffix = ""
         when (type) {
             //拍照/抓拍
             MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE -> {
-                prefix += CAMERA_FILE_PATH
+                prefix += "Photo"
                 suffix = ".jpg"
             }
             //录像
             MediaStore.Files.FileColumns.MEDIA_TYPE_VIDEO -> {
-                prefix += VIDEO_FILE_PATH
+                prefix += "Video"
                 suffix = ".mp4"
             }
             //录音
             MediaStore.Files.FileColumns.MEDIA_TYPE_AUDIO -> {
-                prefix += AUDIO_FILE_PATH
+                prefix += "Audio"
                 suffix = ".wav"
             }
             //录屏
             MediaStore.Files.FileColumns.MEDIA_TYPE_PLAYLIST -> {
-                prefix += SCREEN_FILE_PATH
+                prefix += "Screen"
                 suffix = ".mp4"
             }
         }
@@ -68,6 +64,7 @@ object MediaFileUtil {
 
     /**
      * 传入指定大小的文件长度，扫描sd卡空间是否足够
+     * 需有1G的默认大小的空间
      */
     @JvmStatic
     fun scanDisk(space: Long = 1024): Boolean {
@@ -78,28 +75,41 @@ object MediaFileUtil {
     }
 
     /**
-     * 将bitmap存成文件
+     * 将bitmap存成文件至指定目录下
      */
     @JvmStatic
-    fun saveBitToSd(bitmap: Bitmap, path: String?, quality: Int): Boolean {
-        val file = File(path)
-        if (file.exists()) {
-            file.delete()
-        }
-        return try {
-            val fileOutputStream = FileOutputStream(file)
-            bitmap.compress(Bitmap.CompressFormat.JPEG, quality, fileOutputStream)
+    fun saveBitToSd(bitmap: Bitmap, root: String = Constants.APPLICATION_FILE_PATH + "/图片", quality: Int = 100) {
+        val filePath: String
+        try {
+            val file = File(root)
+            if (!file.mkdirs()) file.createNewFile()//需要权限
+            filePath = "$root/" + DateUtil.getDateTimeStr(EN_YMDHMS, Date()) + ".jpg"
+            val fileOutputStream = FileOutputStream(filePath)
+            bitmap.compress(Bitmap.CompressFormat.PNG, quality, fileOutputStream)
             fileOutputStream.flush()
             fileOutputStream.close()
-            LogUtil.i(TAG, "图片已经保存!")
-            true
-        } catch (e: FileNotFoundException) {
-            LogUtil.e(TAG, "图片保存失败!")
-            false
-        } catch (e: IOException) {
-            LogUtil.e(TAG, "图片保存失败!")
-            false
+        } catch (ignored: Exception) {
+        } finally {
+            bitmap.recycle()
         }
+//        val file = File(path)
+//        if (file.exists()) file.delete()
+//        return try {
+//            val fileOutputStream = FileOutputStream(file)
+//            bitmap.compress(Bitmap.CompressFormat.JPEG, quality, fileOutputStream)
+//            fileOutputStream.flush()
+//            fileOutputStream.close()
+//            LogUtil.i(TAG, "图片已经保存!")
+//            true
+//        } catch (e: FileNotFoundException) {
+//            LogUtil.e(TAG, "图片保存失败!")
+//            false
+//        } catch (e: IOException) {
+//            LogUtil.e(TAG, "图片保存失败!")
+//            false
+//        } finally {
+//            bitmap.recycle()
+//        }
     }
 
 }
