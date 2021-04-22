548eed651ac9ce50ee0cc6eaa628a8c80cf72a76
王彦彬 <wyb@dataqin.com>
2021-04-22 14:16:40 +0800

    字体引用规整

--- lib_pay/src/main/java/com/dataqin/pay/utils/alipay/PayResult.kt
+++ lib_pay/src/main/java/com/dataqin/pay/utils/alipay/PayResult.kt
@@ -15,19 +15,13 @@ class PayResult {
         if (TextUtils.isEmpty(rawResult)) return
         val resultParams = rawResult!!.split(";".toRegex()).toTypedArray()
         for (resultParam in resultParams) {
-            if (resultParam.startsWith("resultStatus")) {
-                resultStatus = gatValue(resultParam, "resultStatus")
-            }
-            if (resultParam.startsWith("result")) {
-                result = gatValue(resultParam, "result")
-            }
-            if (resultParam.startsWith("memo")) {
-                memo = gatValue(resultParam, "memo")
-            }
+            if (resultParam.startsWith("resultStatus")) resultStatus = gatValue(resultParam, "resultStatus")
+            if (resultParam.startsWith("result")) result = gatValue(resultParam, "result")
+            if (resultParam.startsWith("memo")) memo = gatValue(resultParam, "memo")
         }
     }
 
-    private fun gatValue(content: String, key: String): String? {
+    private fun gatValue(content: String, key: String): String {
         val prefix = "$key={"
         return content.substring(content.indexOf(prefix) + prefix.length, content.lastIndexOf("}"))
     }
