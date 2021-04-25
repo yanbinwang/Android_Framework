fe01d5d4e3ebcdfce7d29819b46a5b1ffe882967
王彦彬 <wyb@dataqin.com>
2021-04-25 14:14:26 +0800

    弹窗动画规整

--- lib_map/src/main/java/com/dataqin/map/service/MapReceiver.kt
+++ lib_map/src/main/java/com/dataqin/map/service/MapReceiver.kt
@@ -1,15 +1,18 @@
 package com.dataqin.map.service
 
+import android.annotation.SuppressLint
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
 import androidx.core.app.ActivityCompat
 import com.dataqin.common.bus.RxBus
 import com.dataqin.common.bus.RxEvent
 import com.dataqin.common.constant.Constants
-import com.dataqin.common.utils.NetWorkUtil
 import com.yanzhenjie.permission.runtime.Permission
 
 /**
@@ -23,21 +26,41 @@ import com.yanzhenjie.permission.runtime.Permission
  *
  *  unregisterReceiver(aMapReceiver)
  */
+@SuppressLint("MissingPermission")
 class MapReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context?, intent: Intent?) {
-        //如果网络状态发生变化则需要重新定位-具备权限才会发送对应广播
-        if (ConnectivityManager.CONNECTIVITY_ACTION == intent?.action) {
-            val netWorkState = NetWorkUtil.getNetWorkState()
-            if (-1 != netWorkState) {
-                var granted = true
-                for (index in Permission.Group.LOCATION.indices) {
-                    if (PackageManager.PERMISSION_GRANTED != ActivityCompat.checkSelfPermission(context!!, Permission.Group.LOCATION[index])) {
-                        granted = false
+        (context?.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager).registerNetworkCallback(NetworkRequest.Builder().build(), object : ConnectivityManager.NetworkCallback() {
+                override fun onCapabilitiesChanged(network: Network?, networkCapabilities: NetworkCapabilities?) {
+                    super.onCapabilitiesChanged(network, networkCapabilities)
+                    if (networkCapabilities!!.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)) {
+                        var granted = true
+                        for (index in Permission.Group.LOCATION.indices) {
+                            if (PackageManager.PERMISSION_GRANTED != ActivityCompat.checkSelfPermission(context, Permission.Group.LOCATION[index])) {
+                                granted = false
+                            }
+                        }
+                        if (granted) RxBus.instance.post(RxEvent(Constants.APP_MAP_CONNECTIVITY))
                     }
                 }
-                if (granted) RxBus.instance.post(RxEvent(Constants.APP_MAP_CONNECTIVITY))
-            }
-        }
+            })
+//        //如果网络状态发生变化则需要重新定位-具备权限才会发送对应广播
+//        if (ConnectivityManager.CONNECTIVITY_ACTION == intent?.action) {
+//            val netWorkState = NetWorkUtil.getNetWorkState()
+//            if (-1 != netWorkState) {
+//                var granted = true
+//                for (index in Permission.Group.LOCATION.indices) {
+//                    if (PackageManager.PERMISSION_GRANTED != ActivityCompat.checkSelfPermission(
+//                            context!!,
+//                            Permission.Group.LOCATION[index]
+//                        )
+//                    ) {
+//                        granted = false
+//                    }
+//                }
+//                if (granted) RxBus.instance.post(RxEvent(Constants.APP_MAP_CONNECTIVITY))
+//            }
+//        }
     }
+
 }
