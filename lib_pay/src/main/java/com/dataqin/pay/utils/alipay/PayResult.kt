c90fb1c4eae81856cc41bafc133600e3a36b2175
王彦彬 <wyb@dataqin.com>
2021-05-14 16:44:19 +0800

    gradle升级

--- lib_pay/src/main/java/com/dataqin/pay/utils/alipay/PayResult.kt
+++ lib_pay/src/main/java/com/dataqin/pay/utils/alipay/PayResult.kt
@@ -6,18 +6,19 @@ import android.text.TextUtils
  *  Created by wangyanbin
  *  支付宝支付參數类
  */
-class PayResult {
+class PayResult(rawResult: String?) {
     var resultStatus: String? = null
     var result: String? = null
     var memo: String? = null
 
-    constructor(rawResult: String?) {
-        if (TextUtils.isEmpty(rawResult)) return
-        val resultParams = rawResult!!.split(";".toRegex()).toTypedArray()
-        for (resultParam in resultParams) {
-            if (resultParam.startsWith("resultStatus")) resultStatus = gatValue(resultParam, "resultStatus")
-            if (resultParam.startsWith("result")) result = gatValue(resultParam, "result")
-            if (resultParam.startsWith("memo")) memo = gatValue(resultParam, "memo")
+    init {
+        if (!TextUtils.isEmpty(rawResult)) {
+            val resultParams = rawResult!!.split(";".toRegex()).toTypedArray()
+            for (resultParam in resultParams) {
+                if (resultParam.startsWith("resultStatus")) resultStatus = gatValue(resultParam, "resultStatus")
+                if (resultParam.startsWith("result")) result = gatValue(resultParam, "result")
+                if (resultParam.startsWith("memo")) memo = gatValue(resultParam, "memo")
+            }
         }
     }
 
