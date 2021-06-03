1932bfa12afcb5dcff9d1d082ed37021d7ad94b4
王彦彬 <wyb@dataqin.com>
2021-06-03 16:49:21 +0800

    顶层函数升级

--- lib_common/src/main/java/com/dataqin/common/utils/helper/permission/PermissionHelper.kt
+++ lib_common/src/main/java/com/dataqin/common/utils/helper/permission/PermissionHelper.kt
@@ -27,8 +27,7 @@ class PermissionHelper(context: Context) {
         Permission.Group.LOCATION,//定位
         Permission.Group.CAMERA,//拍摄照片，录制视频
         Permission.Group.MICROPHONE,//录制音频(腾讯x5)
-        Permission.Group.STORAGE//访问照片。媒体。内容和文件
-    )
+        Permission.Group.STORAGE)//访问照片。媒体。内容和文件
     private var onPermissionCallBack: OnPermissionCallBack? = null
 
     //检测权限(默认拿全部，可单独拿某个权限组)
