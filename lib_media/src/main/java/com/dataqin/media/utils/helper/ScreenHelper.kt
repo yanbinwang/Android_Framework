35cd363fd50dbeaefd9ab06874e74b9f2a9c0a55
王彦彬 <wyb@dataqin.com>
2021-06-15 13:56:27 +0800

    定位授权方式休整

--- lib_media/src/main/java/com/dataqin/media/utils/helper/ScreenHelper.kt
+++ lib_media/src/main/java/com/dataqin/media/utils/helper/ScreenHelper.kt
@@ -3,7 +3,9 @@ package com.dataqin.media.utils.helper
 import android.app.Activity
 import android.content.Intent
 import android.media.projection.MediaProjectionManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
+import com.dataqin.common.constant.Constants
 import com.dataqin.common.constant.Extras
 import com.dataqin.common.constant.RequestCode
 import com.dataqin.media.service.ScreenRecordService
@@ -15,10 +17,26 @@ import java.lang.ref.WeakReference
  */
 object ScreenHelper {
     private var weakActivity: WeakReference<Activity>? = null
+    var previewWidth = 0
+    var previewHeight = 0
 
     @JvmStatic
     fun initialize(activity: Activity) {
         weakActivity = WeakReference(activity)
+        //获取录屏屏幕宽高，高版本进行修正
+        previewWidth = Constants.SCREEN_WIDTH
+        previewHeight = Constants.SCREEN_HEIGHT
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            val decorView = activity.window.decorView
+            decorView.post {
+                val displayCutout = decorView.rootWindowInsets.displayCutout
+                val rectLists = displayCutout?.boundingRects
+                if (null != rectLists && rectLists.size > 0) {
+                    previewWidth = Constants.SCREEN_WIDTH - displayCutout.safeInsetLeft - displayCutout.safeInsetRight
+                    previewHeight = Constants.SCREEN_HEIGHT - displayCutout.safeInsetTop - displayCutout.safeInsetBottom
+                }
+            }
+        }
     }
 
     /**
@@ -37,7 +55,7 @@ object ScreenHelper {
      */
     @JvmStatic
     fun startScreenResult(resultCode: Int, data: Intent?) {
-        stopScreen()
+//        stopScreen()
         val service = Intent(weakActivity?.get()!!, ScreenRecordService::class.java)
         service.putExtra(Extras.RESULT_CODE, resultCode)
         service.putExtra(Extras.BUNDLE_BEAN, data)
@@ -50,7 +68,7 @@ object ScreenHelper {
      */
     @JvmStatic
     fun stopScreen() {
-        weakActivity?.get()?.stopService(Intent(weakActivity?.get()!!, ScreenRecordService::class.java))//先停止，提高稳定性
+        weakActivity?.get()?.stopService(Intent(weakActivity?.get()!!, ScreenRecordService::class.java))
     }
 
 }
