1c2193b91be695b798566ab7fd329caa186a1167
王彦彬 <wyb@dataqin.com>
2021-06-29 13:38:43 +0800

    文案，判断休整

--- lib_media/src/main/java/com/dataqin/media/service/ScreenShotObserver.kt
+++ lib_media/src/main/java/com/dataqin/media/service/ScreenShotObserver.kt
@@ -16,7 +16,7 @@ import java.io.File
  *  监听产生文件，获取对应路径
  */
 class ScreenShotObserver : ContentObserver(null) {
-//    private var imageNum = 0
+    private var imagePath = ""
     private val context by lazy { BaseApplication.instance?.applicationContext!! }
     private val TAG = "ScreenShotObserver"
 
@@ -29,52 +29,24 @@ class ScreenShotObserver : ContentObserver(null) {
 
     override fun onChange(selfChange: Boolean) {
         super.onChange(selfChange)
-//        val columns = arrayOf(MediaStore.MediaColumns.DATE_ADDED, MediaStore.MediaColumns.DATA)
-//        var cursor: Cursor? = null
-//        try {
-//            cursor = context.contentResolver.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, columns, null, null, MediaStore.MediaColumns.DATE_MODIFIED + " desc")
-//            if (cursor != null) {
-//                val count = cursor.count
-//                if (imageNum == 0) {
-//                    imageNum = count
-//                } else if (imageNum >= count) {
-//                    return
-//                }
-//                imageNum = count
-//                if (cursor.moveToFirst()) {
-//                    //获取监听的路径
-//                    val filePath = cursor.getString(cursor.getColumnIndex(MediaStore.MediaColumns.DATA))
-//                    e(TAG, "filePath:$filePath")
-//                    //判断当前路径是否为图片，是的话捕获当前路径
-//                    val options = BitmapFactory.Options()
-//                    options.inJustDecodeBounds = true
-//                    BitmapFactory.decodeFile(filePath, options)
-//                    if (options.outWidth != -1) {
-//                        e(TAG, " \n生成图片的路径:$filePath\n手机截屏的路径：${File(filePath).parent}")
-//                        RxBus.instance.post(RxEvent(Constants.APP_SCREEN_SHOT_FILE, File(filePath).parent ?: ""))
-//                    }
-//                }
-//            }
-//        } catch (ignored: Exception) {
-//        } finally {
-//            cursor?.close()
-//        }
+        val columns = arrayOf(MediaStore.MediaColumns.DATE_ADDED, MediaStore.MediaColumns.DATA)
         var cursor: Cursor? = null
         try {
-            cursor = context.contentResolver.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, null, null, null, null)
+            cursor = context.contentResolver.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, columns, null, null, MediaStore.MediaColumns.DATE_MODIFIED + " desc")
             if (cursor != null) {
                 if (cursor.moveToFirst()) {
                     //获取监听的路径
-                    val filePath = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Video.Media.DATA))
+                    val filePath = cursor.getString(cursor.getColumnIndex(MediaStore.MediaColumns.DATA))
+                    if(imagePath == filePath) return
+                    imagePath = filePath
                     e(TAG, "filePath:$filePath")
                     //判断当前路径是否为图片，是的话捕获当前路径
-                    val file = File(filePath)
                     val options = BitmapFactory.Options()
                     options.inJustDecodeBounds = true
                     BitmapFactory.decodeFile(filePath, options)
-                    if (options.outWidth != -1 && file.exists()) {
-                        e(TAG, " \n生成图片的路径:$filePath\n手机截屏的路径：${file.parent}")
-                        RxBus.instance.post(RxEvent(Constants.APP_SCREEN_SHOT_FILE, file.parent ?: ""))
+                    if (options.outWidth != -1) {
+                        e(TAG, " \n生成图片的路径:$filePath\n手机截屏的路径：${File(filePath).parent}")
+                        RxBus.instance.post(RxEvent(Constants.APP_SCREEN_SHOT_FILE, File(filePath).parent ?: ""))
                     }
                 }
             }
@@ -82,6 +54,29 @@ class ScreenShotObserver : ContentObserver(null) {
         } finally {
             cursor?.close()
         }
+//        var cursor: Cursor? = null
+//        try {
+//            cursor = context.contentResolver.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, null, null, null, null)
+//            if (cursor != null) {
+//                if (cursor.moveToFirst()) {
+//                    //获取监听的路径
+//                    val filePath = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Video.Media.DATA))
+//                    e(TAG, "filePath:$filePath")
+//                    //判断当前路径是否为图片，是的话捕获当前路径
+//                    val file = File(filePath)
+//                    val options = BitmapFactory.Options()
+//                    options.inJustDecodeBounds = true
+//                    BitmapFactory.decodeFile(filePath, options)
+//                    if (options.outWidth != -1 && file.exists()) {
+//                        e(TAG, " \n生成图片的路径:$filePath\n手机截屏的路径：${file.parent}")
+//                        RxBus.instance.post(RxEvent(Constants.APP_SCREEN_SHOT_FILE, file.parent ?: ""))
+//                    }
+//                }
+//            }
+//        } catch (ignored: Exception) {
+//        } finally {
+//            cursor?.close()
+//        }
     }
 
     /**
