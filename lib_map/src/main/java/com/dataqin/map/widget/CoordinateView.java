f8153bab7d032987a032637ebdd78a5212f391fe
王彦彬 <wyb@dataqin.com>
2021-07-14 11:00:56 +0800

    广告控件修整

--- lib_map/src/main/java/com/dataqin/map/widget/CoordinateView.java
+++ lib_map/src/main/java/com/dataqin/map/widget/CoordinateView.java
@@ -3,6 +3,8 @@ package com.dataqin.map.widget;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.widget.ImageView;
 
@@ -20,32 +22,61 @@ public class CoordinateView extends ImageView {
 
     public CoordinateView(Context context) {
         super(context);
-        initialize();
+//        initialize();
     }
 
     public CoordinateView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        initialize();
+//        initialize();
     }
 
     public CoordinateView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        initialize();
+//        initialize();
     }
 
-    private void initialize() {
-        setBackgroundResource(R.drawable.animation_coordinate);
-        animationDrawable = (AnimationDrawable) getBackground();
-    }
+//    private void initialize() {
+//        setBackgroundResource(R.drawable.animation_coordinate);
+//        animationDrawable = (AnimationDrawable) getBackground();
+//    }
+
+//    @Override
+//    protected void onDetachedFromWindow() {
+//        super.onDetachedFromWindow();
+//        animationDrawable.stop();
+//        for (int i = 0; i < animationDrawable.getNumberOfFrames(); i++) {
+//            Drawable frame = animationDrawable.getFrame(i);
+//            if (frame instanceof BitmapDrawable) {
+//                ((BitmapDrawable) frame).getBitmap().recycle();
+//            }
+//            frame.setCallback(null);
+//        }
+//        animationDrawable.setCallback(null);
+//    }
 
     public void start() {
+//        stop();
+//        animationDrawable.start();
         stop();
+        setBackgroundResource(R.drawable.animation_coordinate);
+        animationDrawable = (AnimationDrawable) getBackground();
         animationDrawable.start();
     }
 
     public void stop() {
-        animationDrawable.stop();
-        animationDrawable.selectDrawable(0);
+//        animationDrawable.stop();
+//        animationDrawable.selectDrawable(0);
+        if (null != animationDrawable) {
+            animationDrawable.stop();
+            for (int i = 0; i < animationDrawable.getNumberOfFrames(); i++) {
+                Drawable frame = animationDrawable.getFrame(i);
+                if (frame instanceof BitmapDrawable) {
+                    ((BitmapDrawable) frame).getBitmap().recycle();
+                }
+                frame.setCallback(null);
+            }
+            animationDrawable.setCallback(null);
+        }
     }
 
 }
