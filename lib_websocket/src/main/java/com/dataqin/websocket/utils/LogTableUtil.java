5f71a951ac49d6228f5a80c62062564b28f35b83
王彦彬 <wyb@dataqin.com>
2022-06-09 14:18:53 +0800

    更新gradle以及播放器工具类的一些设置

--- lib_websocket/src/main/java/com/dataqin/websocket/utils/LogTableUtil.java
+++ lib_websocket/src/main/java/com/dataqin/websocket/utils/LogTableUtil.java
@@ -2,51 +2,54 @@ package com.dataqin.websocket.utils;
 
 import android.util.Log;
 
+import com.dataqin.websocket.BuildConfig;
+
 /**
  * Logable 默认实现类
  * <p>
  * Created by ZhangKe on 2019/4/29.
  */
 public class LogTableUtil implements LogTableImpl {
+    private final boolean debug = BuildConfig.ISDEBUG;
 
     @Override
     public void v(String tag, String msg) {
-        Log.v(tag, msg);
+        if (debug) Log.v(tag, msg);
     }
 
     @Override
     public void v(String tag, String msg, Throwable tr) {
-        Log.v(tag, msg, tr);
+        if (debug) Log.v(tag, msg, tr);
     }
 
     @Override
     public void d(String tag, String text) {
-        Log.d(tag, text);
+        if (debug) Log.d(tag, text);
     }
 
     @Override
     public void d(String tag, String text, Throwable tr) {
-        Log.d(tag, text, tr);
+        if (debug) Log.d(tag, text, tr);
     }
 
     @Override
     public void i(String tag, String text) {
-        Log.i(tag, text);
+        if (debug) Log.i(tag, text);
     }
 
     @Override
     public void i(String tag, String text, Throwable tr) {
-        Log.i(tag, text, tr);
+        if (debug) Log.i(tag, text, tr);
     }
 
     @Override
     public void e(String tag, String text) {
-        Log.e(tag, text);
+        if (debug) Log.e(tag, text);
     }
 
     @Override
     public void e(String tag, String msg, Throwable tr) {
-        Log.e(tag, msg, tr);
+        if (debug) Log.e(tag, msg, tr);
     }
 
 }
